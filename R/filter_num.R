26131844c44348ccee5aad3b175c60c41dcad341
wkg <whkygl@163.com>
2024-07-18 21:12:14 +0800

    filter_num; test

--- R/filter_num.R
+++ R/filter_num.R
@@ -1,11 +1,45 @@
-#' filter_num
-#' @description Used to filter species whose quantity or discovery rate is higher than num. If num is greater than 1, filter by quantity; if num is less than 1, filter by discovery rate.
-#' @param data easynem type data.
-#' @param num Filter threshold value.
-#' @param ... Other parameters.
-#' @return An easynem object.
+#' Filter easynem tab by discovery rate or abundance
+#'
+#' The \code{filter_num()} is used to filter the rows of the easynem \code{tab}
+#' by abundance or discovery rate. If \code{num>1}, filter by abundance, \code{num}
+#' is the lowest abundance of the \code{tab}; if \code{num<1}, filter by discovery
+#' rate, \code{num} is the lowest discovery rate of the \code{tab}.
+#'
+#' To facilitate code interpretation, it is recommended to use the pipe symbol
+#' [`|>`] to connect functions:
+#'
+#' ```
+#' nem_filter <- nem |> filter_num(target = meta, num = 0.85)
+#' ```
+#' ```
+#' nem_filter <- nem |> filter_num(target = meta, num = 500)
+#' ```
+#'
+#' @usage filter_num(data, num)
+#'
+#' @param data An \code{\link{easynem-class}} data.
+#' @param num Filter threshold value. If \code{num>1}, filter by abundance, \code{num}
+#' is the lowest abundance of the \code{tab}; if \code{num<1}, filter by discovery
+#' rate, \code{num} is the lowest discovery rate of the \code{tab}.
+#'
+#' @return An \code{\link{easynem-class}} data.The results of \code{tab}, \code{tax},
+#' and \code{meta} are the retention values after filtering the \code{tab} by abundance
+#' or discovery rate.
+#'
+#' @seealso
+#' Other functions in this package for filtering and transforming data sets:
+#' \code{\link{filter_name}}, \code{\link{trans_formula}}, \code{\link{trans_formula_v}},
+#' \code{\link{trans_name}}, \code{\link{trans_norm}}, \code{\link{trans_rare}},
+#' \code{\link{trans_combine}}
+#'
 #' @export
-filter_num  <- function(data, num, ...){
+#' @examples
+#' nem <- read_nem2(tab = nemtab, tax = nemtax, meta = nemmeta)
+#' nem_filter <- nem |> filter_num(num = 0.9)
+#' show(nem_filter)
+#' nem_filter <- nem |> filter_num(num = 1000)
+#' show(nem_filter)
+filter_num  <- function(data, num){
   result = data@tab
   colname = colnames(result)[1]
   result2 = data@tax
@@ -30,9 +64,9 @@ filter_num  <- function(data, num, ...){
   colnames(tab)[1] = "SampleID"
   meta = data@meta
   colnames(meta)[1] = "SampleID"
-  if(any(names(tab[,-1]) %in% names(meta[,-1]))){
-    dif = dplyr::setdiff(names(meta), names(tab))
-    meta = meta[,c("SampleID", dif)]
+  if(any(names(tab)[-1] %in% names(meta)[-1])){
+    char_columns <- sapply(meta, is.character)
+    meta = meta[,char_columns]
     meta = merge(meta, tab, by = "SampleID")
   } else {
     meta = merge(meta, tab, by = "SampleID")
--- data-raw/0easynem_test/filter_name.R
+++ data-raw/0easynem_test/filter_name.R
@@ -8,3 +8,12 @@ nem_filter <- easynem |> filter_name(target = tab, OTUID == "Mesorhabditis")
 nem_filter
 nem_filter <- easynem |> filter_name(target = tax, Family == "Rhabditidae")
 nem_filter
+easynem <- read_nem(tab = easynem_example("nemtab1.csv"),
+                    tax = easynem_example("nemtax1.csv"),
+                    meta = easynem_example("nemmeta1.csv"))
+nem_filter <- easynem |> filter_name(target = meta, season == "Summer")
+nem_filter
+nem_filter <- easynem |> filter_name(target = tab, OTUID == "ASV_5")
+nem_filter
+nem_filter <- easynem |> filter_name(target = tax, Genus == "Meloidogyne")
+nem_filter
