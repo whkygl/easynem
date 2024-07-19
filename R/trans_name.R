be0b4f8d1c9b27a58481c0e14733236d50964f1c
wkg <whkygl@163.com>
2024-07-19 15:08:17 +0800

    trans_name; document

--- R/trans_name.R
+++ R/trans_name.R
@@ -1,11 +1,54 @@
-#' trans_name
-#' @description Used to sum species abundance tables grouped by taxonomy table.
-#' @param data easynem type data.
-#' @param taxonomy Species classification name.
-#' @param ... Other parameters.
-#' @return An easynem object.
+#' Reorganize the easynem's tab by taxonomic name
+#'
+#' The \code{trans_name()} is used to re-summarize the nematode abundance table
+#' by nematode taxonomy table.
+#'
+#' To facilitate code interpretation, it is recommended to use the pipe symbol
+#' [`|>`] to connect functions:
+#'
+#' ```
+#' nem_trans <- nem |> trans_name(Family)
+#' ```
+#'
+#' @section Feedings:
+#'
+#' Since the nematode taxonomy table is automatically associated with the nematode
+#' database (\code{\link{nem_database}}) including \code{feeding} and \code{cp_value}
+#' when reading data through \code{\link{read_nem}} or \code{\link{read_nem2}},
+#' \code{feeding} can also be passed as a parameter to \code{trans_name()}. The
+#' corresponding relationship between the feeding value and the actual nematode
+#' feeding habits is as follows:
+#'
+#' * `feeding = 1`, plant feeding
+#' * `feeding = 2`, fungal hyphal feeding
+#' * `feeding = 3`, bacterial feeding
+#' * `feeding = 4`, substrate ingestion
+#' * `feeding = 5`, predation (including specialist predators of nematodes)
+#' * `feeding = 6`, eucaryote feeding
+#' * `feeding = 7`, dispersal stages or animal parasites
+#' * `feeding = 8`, omnivory (including general predators of nematodes)
+#'
+#' @usage trans_name(data, taxonomy)
+#'
+#' @param data An \code{\link{easynem-class}} data.
+#' @param taxonomy Nematode taxonomic name or other nematode attributes.
+#'
+#' @return A reclassified and aggregated \code{\link{easynem-class}}.
+#'
+#' @seealso
+#' Other functions in this package for filtering and transforming data sets:
+#' \code{\link{filter_name}}, \code{\link{trans_formula}}, \code{\link{trans_formula_v}},
+#' \code{\link{filter_num}}, \code{\link{trans_norm}}, \code{\link{trans_rare}},
+#' \code{\link{trans_combine}}
+#'
 #' @export
-trans_name <- function(data, taxonomy, ...){
+#' @examples
+#' nem <- read_nem2(tab = nemtab, tax = nemtax, meta = nemmeta)
+#' nem_trans <- nem |> trans_name(Family)
+#' show(nem_trans)
+#' nem_trans <- nem |> trans_name(feeding)
+#' show(nem_trans)
+trans_name <- function(data, taxonomy){
   nametax = deparse(substitute(taxonomy))
   tax = data@tax
   tab = data@tab
@@ -36,9 +79,9 @@ trans_name <- function(data, taxonomy, ...){
   colnames(tab)[1] = "SampleID"
   meta = data@meta
   colnames(meta)[1] = "SampleID"
-  if(any(names(tab[,-1]) %in% names(meta[,-1]))){
-    dif = dplyr::setdiff(names(meta), names(tab))
-    meta = meta[,c("SampleID", dif)]
+  if(any(names(tab)[-1] %in% names(meta)[-1])){
+    char_columns <- sapply(meta, is.character)
+    meta = meta[,char_columns]
     meta = merge(meta, tab, by = "SampleID")
   } else {
     meta = merge(meta, tab, by = "SampleID")
--- data-raw/0easynem_test/trans_formula_v.R
+++ data-raw/0easynem_test/trans_formula_v.R
@@ -1,6 +1,6 @@
 devtools::install_github("whkygl/easynem")
 library(easynem)
-?trans_formula
+?trans_formula_v
 easynem <- read_nem2(tab = nemtab, tax = nemtax, meta = nemmeta)
-nem_trans <- easynem |> trans_formula(Mesorhabditis, ~log(x+1))
+nem_trans <- easynem |> trans_formula_v(easynem@tab$OTUID, ~log(x+1))
 nem_trans
